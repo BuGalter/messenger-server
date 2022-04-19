6eb84ae3562cb2ee0a6d85343be36aa1ba9383cf
BuGalter <wardoc78@gmail.com>
2022-04-19 22:48:12 +0300

    feat: Added unum for errors

---
+++ src/server/utils/errors.ts
@@ -0,0 +1,8 @@
+export enum Errors {
+  // Invalid payload errors (400)
+  InvalidPayload = 400000,
+  // Not found (404)
+  NotFound = 404000,
+  // Internal Server Error (500)
+  ServerError = 500000,
+}
